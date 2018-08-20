4e5898ea6afe3013f6035e1faa73c3fe73b6a0b1
akandadai <anantkandadai.gmail.com>
2018-08-20 12:29:13 -0400

    fixed insert into

--- DB/csv-to-sql.py
+++ DB/csv-to-sql.py
@@ -7,12 +7,13 @@ try:
 
 	data=pd.read_csv(args[1])
 	table=args[2]
-	output=open(args[3], 'w')
 	insert_stmt_i = """INSERT INTO %s(%s)
-VALUES(%s);"""
-	inserts = '\n'.join([insert_stmt_i % (table, ', '.join(data.columns), ', '.join(data.loc[i])) for i in data.index])
-	output.write(inserts)
-	output.close()
+VALUES"""
+	insert_data_i = "'%s'," * len(data.columns)
+	insert_data_i = insert_data_i[:-1]
+	insert_stmt_i += '(' + insert_data_i + ');'
+	inserts = '\n'.join([insert_stmt_i % ((table, ', '.join(data.columns)) + tuple(data.loc[i])) for i in data.index])
+	print(inserts)
 except:
-	print("3 arguments reuired in this order: csv filename, tablename, output file. This assumes the csv has headers with the name of the required columns")
+	print("3 arguments reuired in this order: csv filename, tablename. This assumes the csv has headers with the name of the required columns")
 
